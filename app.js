c7cb8cd92e7aebf2ac39f11cfc7a197e9266e6be
kibriah <hsnkib@HKpc.localdomain>
2020-01-25 23:00:48 -0500

    roughly done node app on the back end with integrated date api

--- app.js
+++ app.js
@@ -3,14 +3,22 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/deltahacks");
+var request = require('request');
+request('http://worldtimeapi.org/api/timezone/Europe/London.txt',function(error,response,body){
+    if(!error && response.statusCode==200){
+        var Data = JSON.parse(body);
+    }});
+
+
+
 
 var personSchema = new mongoose.Schema({
-    firstname: String,
-    lastname: String,
     email: String,
     password: String,
     entry: Object
 });
+
+
 
 var Person = mongoose.model("Person",personSchema);
 
@@ -19,58 +27,75 @@ app.get("/",function(req,res){
 });
 
 var currentperson;
+
 app.post("/signup",function(req,res){
     var newperson = new Person({
-        firstname: req.body.firstname,
-        lastname: req.body.lastname,
         email:req.body.email,
         password:req.body.password,
         entry: {}
-    })
+    });
     newperson.save(function(err,human){
         if(err){
             console.log("there is an error");
+        }else{
+            console.log(human);
         }
     });
     currentperson = newperson;
     res.redirect("/homepage.ejs");
 });
 
+
 app.post("/login",function(req,res){
     var useremail = req.body.email;
-    var userpassword = req.body.password;
     Person.find({}, function(err,people){
         if(err){
             console.log(err);
         }else{
             for (person in people){
-                if (person.email == useremail){
+                if (person.email === useremail){
                     currentperson = person;
                     res.redirect("/homepage.ejs");
                 }
             }
-            res.alert("You must sign up first");
             res.redirect("/signup.ejs");
         }
-    }));
+    })});
+
 
 app.get("/homepage",function(req,res){
-    res.render("homepage.ejs",firstname:currentperson.firstname);
+    res.render("homepage.ejs",{firstname:currentperson.email});
     //so the firstname thing above is basically a variable inputted into that HTML file which ill turn to a ejs file
 });
 
+
 app.get("/howfeeling",function(req,res){
     res.render("feeling.ejs");
-})
+});
 
-app.post("/howfeelingsubmitted",function(req,res{
-    currentperson.
-}))
+
+app.get("/whatgratefulfor",function(req,res){
+    res.render("grateful.ejs");
+});
+
+
+app.post("/whatgratefulforsubmitted",function(req,res){
+    var feeling = req.body.feeling;
+    date = (Data.utc_datetime).slice(0,10);
+    currentperson.entry.date = feeling;
+    Person.findOneAndUpdate({email:currentperson.email},{entry:currentperson.entry});
+});
+
 
 app.get("/motivquote",function(req,res){
     var motivequote = random of quotes
     res.render("motivationalquote.ejs",{embeddedquote : motivequote});
-})
+});
+
+
+app.get("/pastgratefuls",function(req,res){
+    res.render("pastgratefuls.ejs",{objectfortable: currentperson.entry});
+});
 
 
 
